a959abdae7288f5c3ce6abe90ded284680ba3f12
Julian Borray <julian.borray@bizagi.com>
2019-08-26 23:37:26 -0500

    get paginado

--- modules/baseModule/services/ActionsService.js
+++ modules/baseModule/services/ActionsService.js
@@ -13,7 +13,41 @@ class ActionsService {
      */
     getData(pageNumber, pageSize, filterBy, filterText){
         return new Promise((resolve,reject) =>{
-            resolve(data);
+            try{
+                pageNumber = parseInt(pageNumber);
+                pageSize = parseInt(pageSize);
+                let newResult = data;
+                let totalRecords = data.length;
+                if (typeof filterBy !== 'undefined' || typeof filterText !== 'undefined') {
+                    newResult = data.filter((item)=>{
+                        if(item[filterBy].toLowerCase().indexOf(filterText)>0){
+                            return true;
+                        }
+                    });
+                    totalRecords = newResult.length;
+                }
+                if (newResult.length > pageSize) {
+                    let end = pageNumber*pageSize-1;
+                    let start = end - (pageSize-1);
+                    let paginateResult = [];
+                    for(let i = start; i<=end; i++) {
+                        if (typeof newResult[i] !== 'undefined'){
+                            paginateResult.push(newResult[i]);
+                        }
+                    }
+                    newResult = paginateResult;
+                }
+
+                resolve({
+                    'content': newResult,
+                    'totalRecords': totalRecords,
+                    'totalPages': Math.floor(totalRecords / pageSize) + 1,
+                    'pageIndex': pageNumber,
+                    'pageSize': pageSize
+                });
+            } catch (e) {
+                reject(e)
+            }
         })
     }
 
